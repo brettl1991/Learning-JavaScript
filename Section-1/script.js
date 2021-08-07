1c05f768cdbe08fe10fe715a99d70943a5bec001
Agnes <brettl1991@gmail.com>
2021-08-07 17:35:27 +0100

    Declaire variables, adding values to them and log their values to the console

--- Section-1/script.js
+++ Section-1/script.js
@@ -5,3 +5,16 @@
 // console.log(40 + 8 + 23 - 10);
 
 // -------------------------------------------------------------
+
+let firstName = "Jonas";
+console.log(firstName);
+
+//Exercise
+let country = "Hungary";
+console.log(country);
+
+let continent = "Europe";
+console.log(continent);
+
+let population = 10000000;
+console.log(population);
