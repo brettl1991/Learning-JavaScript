54f78abf55757795c719b25a23f90a4f9cf537a6
Agnes <brettl1991@gmail.com>
2021-08-07 18:00:14 +0100

    Learning about data types

--- Section-1/script.js
+++ Section-1/script.js
@@ -6,15 +6,40 @@
 
 // -------------------------------------------------------------
 
-let firstName = "Jonas";
-console.log(firstName);
+// let firstName = "Jonas";
+// console.log(firstName);
 
-//Exercise
-let country = "Hungary";
-console.log(country);
+// //Exercise
+// let country = "Hungary";
+// console.log(country);
 
-let continent = "Europe";
-console.log(continent);
+// let continent = "Europe";
+// console.log(continent);
 
-let population = 10000000;
-console.log(population);
+// let population = 10000000;
+// console.log(population);
+
+// ------------------------------------------------------------------------
+
+/*Data types*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+
+javaScriptIsFun = 'YES!';
+console.log(typeof javaScriptIsFun);
+
+let year;//undifined value
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);//javascript console saying this is an object, this does not make any sense, this is an error, bug
+
+
+
