aa0089051bcbe27c83c01b7cdee88c41ff7e1889
Agnes <brettl1991@gmail.com>
2021-08-12 13:45:12 +0100

    learning about basic operators

--- Section-1/script.js
+++ Section-1/script.js
@@ -45,18 +45,48 @@
 
 // Let/Const/Var
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-//using let if we want to reassign a value to a variable - so this variable can be mutated -, 
-//using const that variable not supposed to change at any point - so immutable
-const birthYear = 1991;
+// //using let if we want to reassign a value to a variable - so this variable can be mutated -,
+// //using const that variable not supposed to change at any point - so immutable
+// const birthYear = 1991;
 
-//var is an old way to decleare variables
-var job = 'programmer';
-job = 'teacher'
+// //var is an old way to decleare variables
+// var job = 'programmer';
+// job = 'teacher'
 
-//never decleare a variable like this:
-firstName = 'Agnes'
+// //never decleare a variable like this:
+// firstName = 'Agnes'
 
+//-------------------------------------------------------------------------------------
+//Basic Operators allows us to transform values or combine values
+//Mathematical, aritmethical operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
 
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//2 ** 3 means 2 to the powerof 3 so = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Brettl';
+console.log(firstName + ' ' + lastName);
+
+//assigmnet operator
+let x = 10 + 5; //15
+x += 10; //x = x + 10 = 25
+x *= 4; //x=x*4 = 100
+x++; //x = x +1 = 101
+x--; //x = x - 1 = 100
+console.log(x);
+
+//comparison operators
+console.log(ageJonas > ageSarah); //true
+//we have < > <= >=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > - 2018);
