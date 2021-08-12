f12cdbf4a7d1319879f97d254ff8fd0ba1f9e5dd
Agnes <brettl1991@gmail.com>
2021-08-12 12:13:19 +0100

    learning about var, const, let variables

--- Section-1/script.js
+++ Section-1/script.js
@@ -23,23 +23,40 @@
 
 /*Data types*/
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
 
-console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-console.log(typeof 23);
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
 
-javaScriptIsFun = 'YES!';
-console.log(typeof javaScriptIsFun);
+// javaScriptIsFun = 'YES!';
+// console.log(typeof javaScriptIsFun);
 
-let year;//undifined value
-console.log(typeof year);
+// let year;//undifined value
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);//javascript console saying this is an object, this does not make any sense, this is an error, bug
+// console.log(typeof null);//javascript console saying this is an object, this does not make any sense, this is an error, bug
 
+//----------------------------------------------------------------------------
+
+// Let/Const/Var
+
+let age = 30;
+age = 31;
+
+//using let if we want to reassign a value to a variable - so this variable can be mutated -, 
+//using const that variable not supposed to change at any point - so immutable
+const birthYear = 1991;
+
+//var is an old way to decleare variables
+var job = 'programmer';
+job = 'teacher'
+
+//never decleare a variable like this:
+firstName = 'Agnes'
 
 
