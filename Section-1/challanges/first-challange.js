71e3de1f001bfd8260f234f4d368e09f6f0bbdc5
Agnes <brettl1991@gmail.com>
2021-08-22 15:05:41 +0100

    Printing output to the console using if else statement

--- Section-1/challanges/first-challange.js
+++ Section-1/challanges/first-challange.js
@@ -11,3 +11,11 @@ console.log(BMIMark, BMIJohn);
 
 const markHigherBMI = BMIMark > BMIJohn;
 console.log(markHigherBMI);
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`)
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`)
+}
+
+
