4d2be16ffa112d245c578d97c8ad37d207fb3ac9
brettl1991 <brettl1991@gmail.com>
2022-03-09 12:53:16 +0000

    Basic operator practice

--- Section-1/Practice/script.js
+++ Section-1/Practice/script.js
@@ -1,16 +1,37 @@
-let country = "Hungary";
-console.log(country);
+// let country = "Hungary";
+// console.log(country);
 
-let continent = "Europe";
-console.log(continent);
+// let continent = "Europe";
+// console.log(continent);
 
 let population = 10000000;
-console.log(population);
+// console.log(population);
 
-
-let isIsland = false;
+// let isIsland = false;
 let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+language = "hungarian";
+const country = "United Kingdom";
+const continent = "Europe";
+const isIsland = false;
+// isIsland = true;
+
+//Basic Operators
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " million people speak " +
+  language;
+console.log(description1);
