9f2ce97e192d3c8ef54a19024a9d8353d79619b2
brettl1991 <brettl1991@gmail.com>
2022-03-09 14:39:46 +0000

    Type Conversion and Coercion practice

--- Section-1/Practice/script.js
+++ Section-1/Practice/script.js
@@ -49,3 +49,10 @@ if (population > 33) {
 } else {
   console.log(`${country} population is ${avarage} million below avarage.`);
 }
+
+//Type Conversion and Coercion
+console.log("9" - "5"); //4
+console.log("19" - "13" + "17"); // 617
+console.log("19" - "13" + 17); //23
+console.log("123" < 57); // false
+console.log(5 + 6 + "4" + 9 - 4 - 2); //1143
