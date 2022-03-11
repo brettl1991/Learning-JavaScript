342991a5abd4ebbdf5b4cb575bb0ffa9c267c36b
brettl1991 <brettl1991@gmail.com>
2022-03-11 14:01:31 +0000

    Practicing with function declarations and expressions

--- Section-2/Practice/script.js
+++ Section-2/Practice/script.js
@@ -1,9 +1,31 @@
 //Functions
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people, and it's capital city is ${capitalCity}.`;
-}
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people, and it's capital city is ${capitalCity}.`;
+// }
 
-const descPortugal = describeCountry("Portugal", 10, "Lisbon");
-const descGermany = describeCountry("Germany", 83, "Berlin");
-const descFinland = describeCountry("Finland", 6, "Helsinki");
-console.log(descPortugal, descGermany, descFinland);
+// const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+// const descGermany = describeCountry("Germany", 83, "Berlin");
+// const descFinland = describeCountry("Finland", 6, "Helsinki");
+// console.log(descPortugal, descGermany, descFinland);
+
+// Function Declarations vs. Expressions
+// function percentageOfWorld1(population, country) {
+//   return `${country} about ${
+//     (population / 7900) * 100
+//   }% of the world population.`;
+// // }
+// const germanyPopulation = percentageOfWorld1(83, "Germany");
+// const portugalPopulation = percentageOfWorld1(10, "Portugal");
+// const finlandPopulation = percentageOfWorld1(6, "Finland");
+// console.log(germanyPopulation, portugalPopulation, finlandPopulation);
+
+const percentageOfWorld2 = function (population, country) {
+  return `${country} about ${
+    (population / 7900) * 100
+  }% of the world population.`;
+};
+
+const germanyPopulation = percentageOfWorld2(83, "Germany");
+const portugalPopulation = percentageOfWorld2(10, "Portugal");
+const finlandPopulation = percentageOfWorld2(6, "Finland");
+console.log(germanyPopulation, portugalPopulation, finlandPopulation);
