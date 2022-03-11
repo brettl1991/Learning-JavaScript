8589cbae734f97b805c608f6809f59644a365922
brettl1991 <brettl1991@gmail.com>
2022-03-11 13:07:09 +0000

    Practicing with functions

---
+++ Section-2/Practice/script.js
@@ -0,0 +1,9 @@
+//Functions
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people, and it's capital city is ${capitalCity}.`;
+}
+
+const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+const descGermany = describeCountry("Germany", 83, "Berlin");
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+console.log(descPortugal, descGermany, descFinland);
