ee9efaf3dea2dbf20c60ab076ddfb37cafa15884
brettl1991 <brettl1991@gmail.com>
2022-03-11 15:46:03 +0000

    Learning about functionscallingotherfunctions

---
+++ Section-2/_5FunctionsCallingOtherFunctions.js
@@ -0,0 +1,17 @@
+"use strict";
+
+//Using for many reason, mostly for DRY code!!!
+
+//add a machine that cuts the fruit into 4 pieces
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+//we will use the above machine in our function below, so we call one function inside an other function
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+
+  const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces of orange.`;
+  return juice;
+}
+console.log(fruitProcessor(2, 3));
