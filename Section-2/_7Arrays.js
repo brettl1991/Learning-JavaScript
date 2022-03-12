7dbaa7d7a8ac8d647a0cbb8df7b0a0e3be0612fc
brettl1991 <brettl1991@gmail.com>
2022-03-12 10:30:11 +0000

    Fixing typo

--- Section-2/_7Arrays.js
+++ Section-2/_7Arrays.js
@@ -44,7 +44,7 @@ const age2 = calcAge(years[1]);
 const age3 = calcAge(years[years.length - 1]);
 console.log(age1, age2, age3);
 
-//we can place functio calls into an array because they also produces values
+//we can place function calls into an array because they also produces values
 const ages = [
   calcAge(years[0]),
   calcAge(years[1]),
