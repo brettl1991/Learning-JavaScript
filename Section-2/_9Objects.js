e1ea83cfd23e5f9780e70e2ea4db441065aa8c55
brettl1991 <brettl1991@gmail.com>
2022-03-13 15:59:10 +0000

    Learning about objects

---
+++ Section-2/_9Objects.js
@@ -0,0 +1,13 @@
+"use strict";
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtman",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michel", "Peter", "Steven"],
+};
+
+//with objects we are able to assign a name to each of the values which was impossible with arrays, so we are grouping variables, which are really belongs together
+//the objects has properties (like firstName...), many ways to creating objects, to create with {} like above: we are calling it: the object literal syntax
+//big difference between objects and arrays is that by objects the order of their values does not matter at all when we want to retrive them
+//we should use arrays for more ordered data and objects for more unstructere data and data that we want to name and then retrive from the object based on that name
