3f8f5c3638e9b13a91a3f4a8f2e9922d4cfe3908
brettl1991 <brettl1991@gmail.com>
2022-03-15 12:45:38 +0000

    Learning about for loops

---
+++ Section-2/_12IterationTheForLoop.js
@@ -0,0 +1,20 @@
+"use strict";
+// console.log("Lifting weights repetation 1 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 2 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 3 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 4 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 5 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 6 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 7 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 8 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 9 🏋️‍♂️ ");
+// console.log("Lifting weights repetation 10 🏋️‍♂️ ");
+// //insteda of this above we can write a one line code which will run over and over again util we stop it
+//for loop which has a counter, has 3 parts
+//first an initial value of a counter
+//second part is a logical condition that is evaluated before each iteration of the loop
+//so for loop keeps running while condition is TRUE
+//update the counter
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetation ${rep} 🏋️‍♂️ `);
+}
