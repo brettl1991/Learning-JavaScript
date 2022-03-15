a3fd372d49da1183c392a5ef733d5b3cad089322
brettl1991 <brettl1991@gmail.com>
2022-03-15 11:10:05 +0000

    Extending the knowledge about object methods

--- Section-2/_11ObjectMethods.js
+++ Section-2/_11ObjectMethods.js
@@ -15,11 +15,12 @@ const jonas = {
   // },
   //this keyword or this variable = to the object on which the method called, or = to the object calling the method for DRY
   // calcAge: function () {
-  //   return 2037 - this.birthYear;
+  //   console.log(this); //we are getting back the whole object
+  //   return 2037 - this.birthYear;//we read the birthyear directly from the object, so if we are changing the object name we still can access to birthyear with this, but with jonas.birthyear can not if we are changing the name of the object
   // },
 
   calcAge: function () {
-    this.age = 2037 - this.birthYear;
+    this.age = 2037 - this.birthYear; //to give new property to the object
     return this.age;
   },
 
@@ -30,12 +31,12 @@ const jonas = {
   },
 };
 // console.log(jonas.calcAge(1991)); //or can access with bracket notation as well
-// console.log(jonas["calcAge"](1991));
+// console.log(jonas["calcAge"](1991));but we should not repeat ourself if we have birthyear in the object plus here also
 //directly access from jonas property this function using: this
 //who is calling the function/ jonas
 console.log(jonas.calcAge());
 
-//if we need to access the age multiple times trough our code
+//if we need to access the age multiple times
 console.log(jonas.age);
 console.log(jonas.age);
 console.log(jonas.age);
