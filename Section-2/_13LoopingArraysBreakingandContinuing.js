eae27673b0eaa1c3f52d5249ab0fdc2befe51392
brettl1991 <brettl1991@gmail.com>
2022-03-15 16:14:07 +0000

    Learning about looping, arrays, breaking and continue

--- Section-2/_13LoopingArraysBreakingandContinuing.js
+++ Section-2/_13LoopingArraysBreakingandContinuing.js
@@ -17,6 +17,7 @@ const jonas = [
 
 //loop trough this array, log this 5 elemnts to the console
 //i is a traditional counter variable name, let i = 0 because arrays are 0 based
+//create a new array based on the values in one original array above starting with an empty array
 const types = [];
 for (let i = 0; i < jonas.length; i++) {
   //Reading from jonas array
@@ -27,7 +28,6 @@ for (let i = 0; i < jonas.length; i++) {
   types.push(typeof jonas[i]);
 }
 console.log(types);
-//create a new array based on the values in one original array above starting with an empty array
 
 //Example
 const years = [1991, 2007, 1969, 2020]; //we want to calculate the ages for all these birthyear and want to store them in an array
