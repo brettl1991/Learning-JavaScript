de459cb9ca4f252152fac6da13a3ac575daf97a1
brettl1991 <brettl1991@gmail.com>
2022-03-17 16:56:00 +0000

    Debugging with dev tools

--- Developer-Skills/Debugging.js
+++ Developer-Skills/Debugging.js
@@ -16,3 +16,25 @@ const measureKelvin = function () {
   return kelvin;
 }; //1 IDENTIFY THE BUG
 console.log(measureKelvin());
+
+//Using DEBUGGER
+const calcTempAmplitudeBug = function (temp1, temp2) {
+  const temps = temp1.concat(temp2);
+  console.log(temps);
+  //lets printend we put here 0 by msitake
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    // debugger;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+//1.IDENTIFY THE BUG
+console.log(amplitudeBug);
