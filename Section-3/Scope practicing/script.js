2ffd487a7d0611a2c430f09ba3634f9af24c452c
brettl1991 <brettl1991@gmail.com>
2022-03-23 15:44:47 +0000

    Structuring folders and fixing typo

--- Section-3/Scope practicing/script.js
+++ Section-3/Scope practicing/script.js
@@ -7,8 +7,8 @@ function calcAge(birthYear) {
     console.log(output);
     if (birthYear >= 1981 && birthYear <= 1996) {
       var milenial = true;
-      //CreatingNEW variablewith same nameas outer scope'svariable
-      const firstName = 'Steven'; //js look the variable name in teh current scope so changed jonas to steven
+      //Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven'; //js look the variable name in the current scope so changed jonas to steven
       //reassigningouter scope's variable
       output = 'NEW OUTPUT';
       const str = `Oh, and you're a millenial, ${firstName}`;
@@ -20,7 +20,7 @@ function calcAge(birthYear) {
     }
     // console.log(str); //const and let variable block scoped , so only available inside the block they are created
     console.log(milenial); //return back true, var variables are function scoped, ignoring the block, and still we are in the same function
-    add(2, 3); //not defined, functions are block scoped, only true for struct mode
+    add(2, 3); //not defined, functions are block scoped, only true for strict mode
     console.log(output); //we get NEW OUTPUT, we not cerated we just redifind the variable, otherwise will not give the NEW OUTPUT
   }
   printAge();
@@ -29,5 +29,5 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
-console.log(age); //outer scope can't ahve acces to inner scope, but inner scope can have acces to outer scope
+console.log(age); //outer scope can't have acces to inner scope, but inner scope can have acces to outer scope
 printAge(); //same reason as above
