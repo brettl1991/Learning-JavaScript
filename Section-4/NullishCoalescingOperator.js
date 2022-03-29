1a0d5bf9db424466e8c081038ea38332bdd4f059
brettl1991 <brettl1991@gmail.com>
2022-03-29 13:03:55 +0100

    Learning about nullish operator

---
+++ Section-4/NullishCoalescingOperator.js
@@ -0,0 +1,16 @@
+'use strict';
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+//originally would be 10 as || operator will return true value but will nullish operator, which working with a concept of nullish values(null and undefined)
+const guestsCorrect = restaurant.numGuests ?? 10;
+console.log(guestsCorrect); //getting 0
