0ab4c0cf33fb2eaf55ae063b151d35608f59b1e7
brettl1991 <brettl1991@gmail.com>
2022-04-12 16:59:08 +0100

    Fixing typo

--- Section-5/_7immediatelyInvokedFunctionExpressions.js
+++ Section-5/_7immediatelyInvokedFunctionExpressions.js
@@ -5,7 +5,7 @@ const runOnce = function () {
 };
 runOnce();
 
-//immediately onvoked function expression
+//immediately invoked function expression
 //every time we need to protect our variables from overwritten by anyhow is good to use this
 (function () {
   console.log('This will never run again');
@@ -17,7 +17,7 @@ console.log(isPrivate); //global scope does not have access to innerscope
 //another way
 () => console.log('This will never run again')();
 
-//this is a eason why iife are not is common use nowdays because if we want to create a scope for our data we can put it in a block like below
+//this is the reason why iife are not is common use nowdays because if we want to create a scope for our data we can put it in a block like below
 {
   const isPrivate = 23;
 }
