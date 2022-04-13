f00b014b6a956653146f4c936059b8aa4926fc59
brettl1991 <brettl1991@gmail.com>
2022-04-13 13:04:37 +0100

    Closure challange

---
+++ Section-5/challanges/second-challange.js
@@ -0,0 +1,11 @@
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+//why did this work?
+//because of closure: the header is in the bagpack in the function
