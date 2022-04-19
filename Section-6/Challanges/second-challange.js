64f807050f310ba4d966fed40cd8f99d1f809f28
brettl1991 <brettl1991@gmail.com>
2022-04-19 12:09:43 +0100

    Challange with map, filter and reduce method

---
+++ Section-6/Challanges/second-challange.js
@@ -0,0 +1,17 @@
+'use strict';
+//Data 1: [5, 2, 4, 1, 15, 8, 3]
+const dogAge1 = [5, 2, 4, 1, 15, 8, 3];
+//Data 2: [16, 6, 10, 5, 6, 1, 4]
+const dogAge2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : (16 + age) * 4));
+  console.log(humanAges);
+  const exclude = humanAges.filter(age => age >= 18);
+  console.log(exclude);
+  const avarageHuman =
+    exclude.reduce((acc, age) => acc + age, 0) / exclude.length;
+  console.log(avarageHuman);
+};
+
+calcAverageHumanAge(dogAge1);
