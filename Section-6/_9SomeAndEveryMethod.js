fdb18a3f351c60a00da23edfc10b0b6897b68388
brettl1991 <brettl1991@gmail.com>
2022-04-21 16:00:30 +0100

    Learning about every function and separate callback

--- Section-6/_9SomeAndEveryMethod.js
+++ Section-6/_9SomeAndEveryMethod.js
@@ -7,3 +7,21 @@ console.log(movements.includes(-130)); //true, so includes method to test if an 
 //some method we can specify a condition and check
 const anyDeposit = movements.some(mov => mov > 5000);
 console.log(anyDeposit); //false
+
+//every method, similar like some, difference: only returns true if all of the elemnts satisfy the condition
+console.log(movements.every(mov => mov > 0)); //false
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+console.log(account4.movements.every(mov => mov > 0)); //true
+
+//Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit)); //true
+console.log(movements.every(deposit)); //false
+console.log(movements.filter(deposit)); //[ 200, 450, 3000, 70, 1300 ]
