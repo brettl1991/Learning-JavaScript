0c86ea011604fb4fcf1975b408c2af753a459532
brettl1991 <brettl1991@gmail.com>
2022-04-21 17:49:33 +0100

    Learning about flatMap method

---
+++ Section-6/_10FlatandFlatMapMethod.js
@@ -0,0 +1,98 @@
+'use strict';
+
+//flat mthod: remove nested array and flattened the array
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+//[
+//   1, 2, 3, 4,
+//   5, 6, 7, 8
+// ]
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2)); //with these (inside) we can go nested levels
+
+//[
+//   1, 2, 3, 4,
+//   5, 6, 7, 8
+// ]
+
+//calc all the movents of all the accounts
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const accountMovements = accounts.map(acc => acc.movements);
+console.log(accountMovements);
+
+// [
+//   [
+//      200,  450, -400,
+//     3000, -650, -130,
+//       70, 1300
+//   ],
+//   [
+//     5000,  3400,  -150,
+//     -790, -3210, -1000,
+//     8500,   -30
+//   ],
+//   [
+//      200, -200, 340,
+//     -300,  -20,  50,
+//      400, -460
+//   ],
+//   [ 430, 1000, 700, 50, 90 ]
+// ]
+
+const allMovements = accountMovements.flat();
+console.log(allMovements);
+
+// [
+//   200,  450,  -400,  3000, -650,
+//  -130,   70,  1300,  5000, 3400,
+//  -150, -790, -3210, -1000, 8500,
+//   -30,  200,  -200,   340, -300,
+//   -20,   50,   400,  -460,  430,
+//  1000,  700,    50,    90
+// ]
+
+const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance); //17840
+
+// //easier with chaining
+// const overallBalance = accounts
+//   .map(acc => acc.movements)
+//   .flat()
+//   .reduce((acc, mov) => acc + mov, 0);
+
+//pretty common operation that we first map than we flat, but we can use flatMap method, but this method just go 1 level deep if we need more level we need to use the flat method
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
