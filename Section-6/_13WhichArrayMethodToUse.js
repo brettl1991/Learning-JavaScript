070b024dc8f675a87d4f475aeb845fa82a552179
brettl1991 <brettl1991@gmail.com>
2022-04-22 16:23:29 +0100

    Which array methid to use?

---
+++ Section-6/_13WhichArrayMethodToUse.js
@@ -0,0 +1,50 @@
+'use strict';
+//the question that we need to ask what we want to know?
+
+//  ?TO MUTATE THE ORIGINAL ARRAY
+
+//add to original:            .push (end)
+//                            .unshift (start)
+
+//remove from original:       .pop (end)
+//                            .shift (start)
+//                            .splice (any)
+
+//others: that mutate the original array:
+//                            .reverse()
+//                            .sort()
+//                            .fill()
+
+//?A NEW ARRAY
+
+//computed from original:     .map (loop)
+//filtered using condition:   .filter()
+//portion of original:        .slice()
+//adding original to other:   .concat()
+//flattening the original:    .flat()           .flatMap()
+
+//?AN ARRAY INDEX
+
+//based on value:             .indexOf()
+//based on test condition:    .findIndex()
+
+//?AN ARRAY ELEMENT ITSELF
+
+//based on a test condition:  .find()
+
+//?KNOW IF ARRAY INCLUDES
+
+//based on value:             .includes()
+//based on test condition:    .some()           .every()
+
+//?A NEW STRING
+
+//based on separator string:  .join()
+
+//?TO TRANSFORM TO VALUE
+
+//based on accumulator:       .reduce()       boil down to single value of any type: number, boolean, string or even new array or object
+
+//?TO JUST LOOP ARRAY
+
+//based on callback:          .forEach()      does not create a new array just loops over it
