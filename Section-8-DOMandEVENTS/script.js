f59dd32c3ab4d7d525be490344fc38f14d4db7f2
brettl1991 <brettl1991@gmail.com>
2022-05-04 12:24:13 +0100

    How to select elements

--- Section-8-DOMandEVENTS/script.js
+++ Section-8-DOMandEVENTS/script.js
@@ -29,3 +29,22 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//selecting elements
+console.log(document.documentElement); //in the DOM the entire html elemnet selected by the console
+console.log(document.head);
+console.log(document.body);
+
+//or
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections); //this will return a nodelist which will contain all of the elements that are a section
+
+//or
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); //get ElementsByTagName returns a HTML collection with all the buttons, HTML collection when the DOM changes (like we delete one button) will be updated automatically and going to be 8 button not 9 in total, but with nodelist (quesrySelectorAll) it is not happening, when we try and delete one section, still we get back 4 sections  because the variable was created by the time when a section still was existed
+
+//or
+
+console.log(document.getElementsByClassName('btn')); //this will also return a HTML collection
