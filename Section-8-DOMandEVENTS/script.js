4aaeb5962efabcb5fb30ae60bf144216719ae8be
brettl1991 <brettl1991@gmail.com>
2022-05-04 12:58:41 +0100

    How to create and insert elements

--- Section-8-DOMandEVENTS/script.js
+++ Section-8-DOMandEVENTS/script.js
@@ -36,7 +36,7 @@ console.log(document.head);
 console.log(document.body);
 
 //or
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections); //this will return a nodelist which will contain all of the elements that are a section
 
@@ -48,3 +48,27 @@ console.log(allButtons); //get ElementsByTagName returns a HTML collection with 
 //or
 
 console.log(document.getElementsByClassName('btn')); //this will also return a HTML collection
+
+//Creating and inserting elements
+// .insertAdjacentHTML //quick and easy to use
+
+//or creating elements from scratch
+const message = document.createElement('div'); //this no where to be found in our webpage just we created and we can use to do something, if we want to add on the page we manually need to insert it
+//this element will display cookie messages
+message.classList.add('cookie-message');
+//we can also add text to the element
+message.textContent =
+  'We use cookies for improved functionality and analytics.';
+//we can insert also HTML
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+//insert it to the DOM in to our page in header
+// header.prepend(message); //prepend adding the element as a first child
+//append adding the element as a last child
+header.append(message);
+//a DOM element is unique so always only exist in one place at the time, so with those above we can just insert once
+//but if we want to insert multiple copies of the same element: first we need to copy
+// header.append(message.cloneNode(true)); //all the child elements will be copied, so now we get the same cookie message in both places
+
+// header.before(message); //insert the message as a sybling before the header element
+// header.after(message); //insert the message as a sybling after the header element
