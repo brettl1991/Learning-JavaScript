bf9204816cc3f81a43795a520f5198a509167ff3
brettl1991 <brettl1991@gmail.com>
2022-05-08 14:01:43 +0100

    Activate the dots to show which one is active

--- Section-8-DOMandEVENTS/slidercomponent.js
+++ Section-8-DOMandEVENTS/slidercomponent.js
@@ -24,6 +24,7 @@ const goToSlide = function (slide) {
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
+activateDots(0); //with this the first dot got activated once we reload the page
 goToSlide(0);
 //going to the next slide (active 0%, previous -100%, next one is 100%)
 const nextSlide = function () {
@@ -34,6 +35,7 @@ const nextSlide = function () {
   }
   //CurrentSlide = 1: -100%,0%,100%,200%
   goToSlide(currentSlide);
+  activateDots(currentSlide);
 };
 
 const prevSlide = function () {
@@ -43,6 +45,7 @@ const prevSlide = function () {
     currentSlide--;
   }
   goToSlide(currentSlide);
+  activateDots(currentSlide);
 };
 
 btnRight.addEventListener('click', nextSlide);
@@ -55,3 +58,41 @@ document.addEventListener('keydown', function (e) {
   //or we can use short circuting
   // e.key === 'ArrowLeft' && nextSlide();
 });
+
+////creating the dots underneath the slider
+const dotContainer = document.querySelector('.dots');
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+createDots();
+
+//make dots work using event delegation
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    console.log('DOT');
+    const { slide } = e.target.dataset; //dataset.slide comes from above data-slide, so all the custom data attributes are in the dataset and .slide, but because we destructured .slide it is not need end of dataset
+    //go to the side that we just selected
+    goToSlide(slide);
+    activateDots(slide);
+  }
+});
+
+//activate dots to show which one is going to be the curent slide, no matter we click on the dots or we are using the arrow buttons
+const activateDots = function (slide) {
+  //first we will select all the dots and remove the active class and add only to that one which we are intrested in
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('dots__dot--active'));
+  //select the one we are intrested in based on the data attribute
+  document
+    .querySelector(`.dots__dot[data-slide = "${slide}"]`)
+    .classList.add('dots__dot--active');
+  //we need to add this function to all the necessary places
+};
+
+//one problem when we reload non of the slides are active, to solve add createDots(0) before goToSlide()
