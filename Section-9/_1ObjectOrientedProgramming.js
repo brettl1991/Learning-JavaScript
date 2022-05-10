24c1501b2f1e545a3766cc9532bffea24b49e85f
brettl1991 <brettl1991@gmail.com>
2022-05-10 13:15:34 +0100

    Learning about OOP

---
+++ Section-9/_1ObjectOrientedProgramming.js
@@ -0,0 +1,8 @@
+'use strict';
+//a programming paradigm (style of the code) based on the concept of objects
+//we pack data and the corresponding behavior into one block
+//4 fundamental principles of OOP:
+//1. Abstraction: ignore, hide details that dont matter, allowing us to get an overview of the thing we are implementing
+//2. Encapsulation: keeping properties and methods private inside the class, so they are not accessible from outside the class. Bu some methods can be exposed as a public interface (API)
+//3. Inheritance: making all properties and methods of a certain class available to a child class, this allow us to reuse common logic.
+//4. Polymorphism: a child class can overwrite a method it inherited from a parent class.
