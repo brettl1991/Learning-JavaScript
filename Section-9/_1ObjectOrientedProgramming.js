fcbceefe7d0b897f7c78c5c801cada37824eb1d1
brettl1991 <brettl1991@gmail.com>
2022-05-10 14:37:12 +0100

    OOP in javascript

--- Section-9/_1ObjectOrientedProgramming.js
+++ Section-9/_1ObjectOrientedProgramming.js
@@ -6,3 +6,14 @@
 //2. Encapsulation: keeping properties and methods private inside the class, so they are not accessible from outside the class. But some methods can be exposed as a public interface (API)
 //3. Inheritance: making all properties and methods of a certain class available to a child class, this allow us to reuse common logic.
 //4. Polymorphism: a child class can overwrite a method it inherited from a parent class.
+
+//OOP in JS
+//Instances (Objects) are instantiated from a class
+//in js OOP is prototypes, so objects are linked to a prototype object
+//Prototypal inheritence: the prototype contains methods(behavior) that are accessible to all object linked to that prototype. So basically an instance inheriting from a class!!! Behavior is delegated to the linked prototype object.
+
+//3 ways of implementing prototypal inheritance in js:
+//1. constructor function: technique to create objects from a function, this is how buyil-in objects, like arrays, maps or sets are implemented.
+//2. ES6 classes: modern alternative."Syntetic sugar": behind the scenes, ES6 calasses work like constructor functions. They do not behave like classes like in "classical" OOP.
+//3. Object.create(): easisest and most straightforward way of linking an object to a prototype object.
+//the 4 OOP pillars are still valid here!!!
